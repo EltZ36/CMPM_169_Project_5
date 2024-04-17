c7987971af906d580987282bd1e53cea28900115
Elton Z <poptako9@gmail.com>
2024-04-16 21:11:29 -0700

    main/adding in the knife and blade objects for more adjustability

--- src/knife.js
+++ src/knife.js
@@ -1,34 +1,82 @@
-class Blade{
-    constructor(x,y,size, blade_color, blade_pattern){
+class Blade {
+    constructor(x, y, size) {
         this.x = x
         this.y = y
+        this.size = size
+        this.blade_color = ""
+        this.blade_pattern = ""
+    }
+
+    roll() {
+
+    }
+
+    show() {
+        //priori artifact: 
+        fill(170)
+        //the blade itself 
+        beginShape()
+        //include extra conditions depending on the knife shape
+        //if its chef knife, then do the bezier and stuff like that 
+        vertex(this.x, this.y)
+        vertex(this.x, this.y - 160)
+        vertex(this.x, this.y - 170)
+        bezierVertex(this.x + 5, this.y - 160, this.x + 40, this.y - 130, this.x + 50, this.y)
+        /*vertex(180, 190)
+        vertex(180, 30)
+        vertex(180, 20)
+        bezierVertex(185, 30, 220, 60, 230, 180)
+        vertex(230, 190)*/
+        endShape()
+    }
+
+    setX(x) {
+        this.x = x
+    }
+
+    getX() {
+        return this.x
+    }
+
+    setY(y) {
+        this.y = y
+    }
+
+    getY() {
+        return this.y
+    }
+
+    setSize(size){
         this.size = size 
-        this.blade_color = blade_color
-        this.blade_pattern = blade_pattern
     }
 
-    roll(){
-
-    }
-
-    show(){
-
-    }
-
-    setX(){
-
-    }
-
-    getX(){
-
-    }
-
-    setY(){
-
-    }
-
-    getY(){
-        
+    getSize(){
+        return this.size
     }
 }
 
+class Handle{
+    constructor(x, y, width, height, size){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.size = size 
+    }
+    show(){
+        fill(100);
+        noStroke();
+        //the handle part 
+        rect(this.x, this.y+10, this.width, this.height)
+        //these variables should be adjustable 
+        arc(this.x + 20, this.y + 70, this.width + 20, this.height - 20, 0, PI)
+        fill(255);
+        //holes for the handle 
+        ellipse(this.x + 10, this.y + 30, this.width - 12, this.height - 52)
+        ellipse(this.x + 10, this.y + 54, this.width - 12, this.height - 52)
+        //tang 
+        //different type = different color 
+        fill(180)
+        rect(this.x, this.y, this.width + 30, this.height - 48)
+    }
+}
--- src/sketch.js
+++ src/sketch.js
@@ -4,38 +4,14 @@ function setup(){
 }
 
 function draw(){
-    fill(100);
-    noStroke();
-    //the handle part 
-    rect(180, 200, 20, 50)
-    arc(200, 250, 40, 40, 0, PI)
-    //add in the ellipses
-    fill(255);
-    ellipse(190, 220, 8, 8)
-    ellipse(190, 238, 8, 8)
-    //the tang
-    //fill in the color of the tang 
-    fill(180)
-    rect(180, 190, 50, 15)
-    fill(170)
-    //the blade itself 
-    beginShape()
-    //include extra conditions depending on the knife shape
-    //if its chef knife, then do the bezier and stuff like that 
-    vertex(180, 190)
-    vertex(180, 30)
-    bezierVertex(180, 30, 220,60, 230, 190)
-    vertex(230, 190)
-    endShape()
-    //triangle()
+    var gen_blade = new Blade(180, 190, 2)
+    var gen_handle = new Handle(180, 190, 20, 60,2); 
+    //priori artifact 
+    gen_handle.show()
+    gen_blade.show()
     stroke(0, 150, 0)
     strokeWeight(3)
     //left side 
-    point(180, 145)
-    point(180, 80)
-    point(180, 20)
-    //right side 
-    point(230, 145)
-    point(230, 70)
+    point(180, 190)
 }
 
