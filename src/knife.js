0755ee15a599dee83031e3543e7a0208d29527aa
Elton Zeng <poptako9@gmail.com>
2024-04-18 16:35:25 -0700

    main/adding in the random colors for the knives.

--- src/knife.js
+++ src/knife.js
@@ -5,31 +5,34 @@ class Blade {
         this.size = size
         this.blade_color = ""
         this.blade_pattern = ""
-        this.shape = 0
         this.width = width
         this.height = height
-        this.showHeel = false 
+        this.showHeel = false
+        this.switching = false 
+        this.isGenerated = false 
     }
 
     roll(){
-        if(this.shape == 0){   
+        if(this.showHeel == true){   
             this.width = random(20, 177)
             this.leftYOffset = random(this.width, this.width + 151)
             this.middleOffset = random(20, 51)
         }
-        if(this.shape == 1){
-            //this.leftOffsetBottom =   
-            //this.leftOffsetTop = 
-            //this.bezierOffsetTop = 
-            //this.bezierOffsetControl = 
-            //this.bezierOFfsetBot = 
-            //this.rightOffsetBottom  =
-            //this.leftOffsetTop = 
+        if(this.showHeel == false){
+            this.slightOffset = random(1, 11)
+            this.width = random(20, 121)
+            this.leftYOffset = random(this.width, this.width + 201)
+            this.middleOffset = random(20, 201)
         }
+        this.patternColorRed = random(0, 256)
+        this.patternColorBlue = random(0, 256)
+        this.patternColorGreen = random(0, 256)
+        this.generateBladePattern()
     }
 
     show() {
         //maybe have the people be able to adjust this with a slider??
+        //background(0)
         push()
         scale(this.size)
         translate(-200 * (this.size- 1), -200* (this.size - 1));
@@ -40,7 +43,7 @@ class Blade {
         //if its chef knife, then do the bezier and stuff like that 
         vertex(this.x, this.y)
         vertex(this.x, this.y - this.leftYOffset)
-        if(this.shape == 0){
+        if(this.showHeel == true){
             //+offset or whatever 
             vertex(this.x + this.width, this.y - this.leftYOffset);
             bezierVertex(this.x + this.width, this.y - this.leftYOffset, this.x + this.width +this.middleOffset, this.y - 35, this.x + this.width + 5 , this.y)
@@ -48,27 +51,44 @@ class Blade {
             //fill(230)
             //ellipse(this.x + this.rightXOffset - 200, this.y - this.leftYOffset, 13, 13)
         }
-        if(this.shape == 1){
-            bezierVertex(this.x + 5, this.y - 160, this.x + 40, this.y - 130, this.x + 50, this.y)
+        if(this.showHeel == false){
+            bezierVertex(this.x + 5, this.y - this.leftYOffset, this.x + this.middleOffset, this.y - this.leftYOffset, this.x + this.width, this.y - this.slightOffset)
+            vertex(this.x + this.width, this.y +  this.slightOffset)
         }
         //make sure the handle offset is the correct length too 
         //the limit is 5 and the knife width should be equal to the handle width 
         endShape()
+        /*stroke(255,0, 0)
+        strokeWeight(3)
+        point(this.x + this.width, this.y - 20)
+        stroke(0,255, 0)
+        point(this.x + this.middleOffset, this.y -  this.leftYOffset)
+        stroke(0,0,255)
+        point(this.x + 5, this.y - this.leftYOffset)
+        noStroke()*/ 
         //heel and blade patterns 
         //different type = different color 
         //make the heel just a slightly lower gradient color to help with this 
-        if(this.shape == 0){
+        if(this.showHeel == true){
             fill(210)
             rect(this.x, this.y, 20, this.height - 48)
             //calculate the space between the x width and y width to randomly place triangles around 
             //change fill as a result with random gradients? 
-
         }
-        if(this.shape == 1){
+        if(this.showHeel == false){
             fill(200)
-            rect(this.x, this.y, this.width + 30, this.height - 48)
+            rect(this.x, this.y, this.width, this.height - 48)
         }
         pop()
+        //make the random triangle pattern 
+        fill(this.patternColorRed,this.patternColorBlue ,this.patternColorGreen)
+        for (let i = 0; i < this.triangleArray.length; i++) {
+            triangle(
+              this.triangleArray[i].x1, this.triangleArray[i].y1,
+              this.triangleArray[i].x2, this.triangleArray[i].y2,
+              this.triangleArray[i].x3, this.triangleArray[i].y3
+            );
+        }
     }
 
     setX(x) {
@@ -96,54 +116,85 @@ class Blade {
     }
 
     //get the visibility of the heel 
-    getHeelVis(){
-        return this.showHeel 
+    getHeelStatus(){
+        return this.showHeel
     }
     
-    setHellVis(show){
-        this.showHeel = show 
+    setHeelStatus(){
+        if(this.showHeel == false){
+            this.showHeel = true 
+        }
+        else{
+            this.showHeel = false
+        }
+    }
+
+    getPatternStatus(){
+        return this.isGenerated 
+    }
+
+    setPatternStatus(){
+        if(this.isGenerated == false){
+            this.isGenerated= true 
+        }
+        else{
+            this.isGenerated= false
+        }
     }
 
     /*from gpt: "I want to put a random number of triangles on a rectangle in p5.js. I want to make sure that the triangles don't overlap the boundaries of the rectangle and stay inside of it. "
     "i also want to make sure that there isn't too much overlap between the triangles and their positions
     "*/ 
     generateBladePattern(){
+        this.triangleArray = []
         //maybe make the triangles a random number 
-        var numTriangles = 10; 
+        var numTriangles = random(5, 15); 
+        //if(this.isGenerated != true){
+        if (this.triangleArray.length === 0){
         for (let i = 0; i < numTriangles; i++) {
             let x1, y1, x2, y2, x3, y3;
         do {
-              x1 = random(50, 50 + rectWidth);
-              y1 = random(50, 50 + rectHeight);
-              x2 = random(50, 50 + rectWidth);
-              y2 = random(50, 50 + rectHeight);
-              x3 = random(50, 50 + rectWidth);
-              y3 = random(50, 50 + rectHeight);
+            if(this.showHeel == true){
+              x1 = random(this.x, this.x + this.width);
+              y1 = random(this.y, this.y - this.leftYOffset);
+              x2 = random(this.x , this.x + this.width);
+              y2 = random(this.y, this.y - this.leftYOffset);
+              x3 = random(this.x, this.x + this.width);
+              y3 = random(this.y, this.y - this.leftYOffset);
+            }
         } 
-        while (isOverlapping(x1, y1, x2, y2, x3, y3));
-            triangles.push({x1, y1, x2, y2, x3, y3});
+        while (this.isOverlapping(x1, y1, x2, y2, x3, y3));
+            this.triangleArray.push({x1, y1, x2, y2, x3, y3});
         }
-        for (let i = 0; i < triangles.length; i++) {
-            triangle(
-              triangles[i].x1, triangles[i].y1,
-              triangles[i].x2, triangles[i].y2,
-              triangles[i].x3, triangles[i].y3
-            );
-          }
+        //draw out the triangles 
+        //this.isGenerated = true 
+        //}
+        }
     }
 
     isOverlapping(x1, y1, x2, y2, x3, y3) {
+        // Check if any vertex of the triangle is outside the blade boundaries
+        if (x1 < this.x || x1 > this.x + this.width ||
+            x2 < this.x || x2 > this.x + this.width ||
+            x3 < this.x || x3 > this.x + this.width ||
+            y1 > this.y || y2 > this.y || y3 > this.y) {
+            return true; // Overlapping
+        }
+        return false; // Not overlapping
+    }
+
+    /*isOverlapping(x1, y1, x2, y2, x3, y3) {
         var minDistance = 20; 
-        for (let i = 0; i < triangles.length; i++) {
-          let d1 = dist(x1, y1, triangles[i].x1, triangles[i].y1);
-          let d2 = dist(x2, y2, triangles[i].x2, triangles[i].y2);
-          let d3 = dist(x3, y3, triangles[i].x3, triangles[i].y3);
+        for (let i = 0; i < this.triangleArray.length; i++) {
+          let d1 = dist(x1, y1, this.triangleArray[i].x1, this.triangleArray[i].y1);
+          let d2 = dist(x2, y2, this.triangleArray[i].x2, this.triangleArray[i].y2);
+          let d3 = dist(x3, y3, this.triangleArray[i].x3, this.triangleArray[i].y3);
           if (d1 < minDistance || d2 < minDistance || d3 < minDistance) {
             return true;
           }
         }
         return false;
-    }
+    }*/
 }
 
 class Handle{
--- src/sketch.js
+++ src/sketch.js
@@ -4,21 +4,48 @@ var gen_handle = new Handle(350, 400, 20, 60,1)
 function setup(){
     createCanvas(800, 800)
     gen_blade.roll()
-    noLoop()
+    this.switchButton = createButton('Switch Shape')
+    this.switchButton.position(20, 10)
+    this.switchButton.mousePressed(switchBlade);
+    this.genButton = createButton('Generate')
+    this.genButton.position(150, 10)
+    this.genButton.mousePressed(rollBlade);
+    //this.patternButton = createButton('Changle/Make a Pattern')
+    //this.patternButton.position(250, 10)
+    //this.patternButton.mousePressed(makeBladePattern)
+    gen_blade.generateBladePattern()
+    //noLoop()
 }
 
 function draw(){
     background(245)
     //priori artifact 
-    gen_handle.show()
+    //gen_blade.setHeelStatus()
     gen_blade.show()
+    gen_handle.show()
+    //gen_blade.makeBladePattern()
     //stroke(0, 150, 0)
     //strokeWeight(3)
     //left side 
     //point(180, 190)
 }
 
-function mousePressed(){
-    gen_blade.roll() 
-    loop()
+//functions for rolling and switching the shapes 
+function rollBlade(){
+    gen_blade.roll()
+    gen_blade.setPatternStatus(false)
+    gen_blade.generateBladePattern()
+}
+
+function switchBlade(){
+    gen_blade.setHeelStatus()
+}
+
+function setKnifeSize(){
+    gen_blade.setSize()
+    gen_handle.setSize()
+}
+
+function makeBladePattern(){
+    gen_blade.generateBladePattern()
 }
